50d3e54a57ed3599363115e38cbf8352f617a2d1
KinjiKawaguchi <kawakin0310@icloud.com>
2025-04-22 23:59:37 +0900

    feat: Discord IDでメンバーを取得するユースケースを追加

---
+++ src/application/usecase/GetMemberByDiscordId.ts
@@ -0,0 +1,20 @@
+import type { Member, MemberRepository } from "../../domain";
+import type { IUseCase } from "./BaseUseCase";
+
+export interface GetMemberByDiscordIdInput {
+	discordId: string;
+}
+
+export type GetMemberByDiscordIdOutput = Member | null;
+
+export class GetMemberByDiscordIdUseCase
+	implements IUseCase<GetMemberByDiscordIdInput, GetMemberByDiscordIdOutput>
+{
+	constructor(private readonly memberRepo: MemberRepository) {}
+
+	async execute(
+		input: GetMemberByDiscordIdInput,
+	): Promise<GetMemberByDiscordIdOutput> {
+		return await this.memberRepo.findByDiscordAccountId(input.discordId);
+	}
+}
