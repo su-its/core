b83d4f906a208d2ce8a90125d728d7930e0e27ad
KinjiKawaguchi <kawakin0310@icloud.com>
2025-04-23 00:12:14 +0900

    feat: tsconfig-pathsパッケージを追加し、printuserdata.tsでメンバー型を明示的に指定

--- example/printuserdata.ts
+++ example/printuserdata.ts
@@ -1,14 +1,18 @@
-import { createMemberUseCases } from "../src/";
+import { createMemberUseCases, type Member } from "../src/";
 
 const memberUseCases = createMemberUseCases();
 
 (async () => {
-	const member = await memberUseCases.getMemberByEmail
+	const member: Member | null = await memberUseCases.getMemberByEmail
 		.execute({ email: "kawaguchi.kinji@shizuoka.ac.jp" })
 		.catch((error) => {
 			console.error("メンバー情報の取得に失敗しました:", error);
 			throw error;
 		});
+	if (!member) {
+		console.error("メンバー情報が見つかりませんでした");
+		return;
+	}
 
 	console.log(member);
 })();
