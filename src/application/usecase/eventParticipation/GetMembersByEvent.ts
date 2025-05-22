8a4377e4f681cd43e15e4cb22488df0057dc2755
KinjiKawaguchi <kawakin0310@icloud.com>
2025-05-23 06:50:46 +0900

    feat: イベント参加に関するユースケースを追加

---
+++ src/application/usecase/eventParticipation/GetMembersByEvent.ts
@@ -0,0 +1,32 @@
+import type { Member } from "../../../domain/aggregates/member/Member";
+import type { MemberRepository } from "../../../domain/aggregates/member/MemberRepository";
+import type { EventRepository } from "../../../domain/aggregates/event/EventRepository";
+import { EventNotFoundException } from "../../exceptions/ApplicationExceptions";
+import type { IUseCase } from "../base";
+
+export interface GetMembersByEventInput {
+	eventId: string;
+}
+
+export class GetMembersByEvent
+	implements IUseCase<GetMembersByEventInput, Member[]>
+{
+	constructor(
+		private readonly eventRepository: EventRepository,
+		private readonly memberRepository: MemberRepository,
+	) {}
+
+	async execute(input: GetMembersByEventInput): Promise<Member[]> {
+		const event = await this.eventRepository.findById(input.eventId);
+		if (!event) {
+			throw new EventNotFoundException();
+		}
+		return (
+			await Promise.all(
+				event
+					.getMemberIds()
+					.map((memberId) => this.memberRepository.findById(memberId)),
+			)
+		).filter((member) => member !== null);
+	}
+}
---
+++ src/application/usecase/eventParticipation/GetMembersByExhibit.ts
@@ -0,0 +1,27 @@
+import type { EventRepository } from "../../../domain/aggregates/event/EventRepository";
+import type { Member } from "../../../domain/aggregates/member/Member";
+import type { MemberRepository } from "../../../domain/aggregates/member/MemberRepository";
+import type { IUseCase } from "../base";
+
+export interface GetMembersByExhibitInput {
+	exhibitId: string;
+}
+
+export class GetMembersByExhibit
+	implements IUseCase<GetMembersByExhibitInput, Member[]>
+{
+	constructor(
+		private readonly memberRepository: MemberRepository,
+		private readonly eventRepository: EventRepository,
+	) {}
+
+	async execute(input: GetMembersByExhibitInput): Promise<Member[]> {
+		const event = await this.eventRepository.findByExhibitId(input.exhibitId);
+		const members = await Promise.all(
+			event
+				.getMemberIds()
+				.map((memberId) => this.memberRepository.findById(memberId)),
+		);
+		return members.filter((m): m is Member => m !== null);
+	}
+}
