07ec3cc5c13dc24c3661683a538c4adf0955b9b4
KinjiKawaguchi <kawakin0310@icloud.com>
2025-05-30 21:23:46 +0900

    docs: exampleの修正

--- example/printuserdata.ts
+++ example/printuserdata.ts
@@ -1,18 +1,19 @@
-import { type Member, createMemberUseCases } from "../src/";
+import { createMemberUseCases } from "../src/";
+import type { GetMemberByEmailOutput } from "../src/application";
 
 const memberUseCases = createMemberUseCases();
 
 (async () => {
-	const member: Member | null = await memberUseCases.getMemberByEmail
+	const result = await memberUseCases.getMemberByEmail
 		.execute({ email: "kawaguchi.kinji@shizuoka.ac.jp" })
 		.catch((error) => {
 			console.error("メンバー情報の取得に失敗しました:", error);
 			throw error;
 		});
-	if (!member) {
+	if (!result.member) {
 		console.error("メンバー情報が見つかりませんでした");
 		return;
 	}
 
-	console.log(member);
+	console.log(result.member);
 })();
